f35a52a9996a77a75fa3f00f88bd37df5c26c50f
Shahrayar <shahrayarmalik86@gmail.com>
2021-04-13 17:46:54 +0500

    Class 4 practice

---
+++ Class 4/Code/lib/Solution_Q1.dart
@@ -0,0 +1,18 @@
+// Q1- Take 10 integer inputs from user and store them in a list and print them on screen.
+
+import 'dart:io';
+
+void main() {
+  // List<int> list_ = [];
+  var list_ = [];
+
+  const LOWER_RANGE = 0;
+  const UPPER_RANGE = 10;
+
+  // for (int i = LOWER_RANGE; i < UPPER_RANGE; i++) {
+  //   print("Enter number at index ${i}");
+  //   list_.add(int.parse(stdin.readLineSync()));
+  // }
+
+  print("Elements you entered in list are:\n${list_}");
+}
