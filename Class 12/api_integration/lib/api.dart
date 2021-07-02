7374ff9b7e1622c0ffbe37e1d7b3368b9853f306
Shahrayar <shahrayarmalik86@gmail.com>
2021-07-02 17:42:59 +0500

    Class 12 code

--- Class 12/api_integration/lib/api.dart
+++ Class 12/api_integration/lib/api.dart
@@ -1,15 +1,61 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Api extends StatefulWidget {
-  const Api({Key? key}) : super(key: key);
-
   @override
   _ApiState createState() => _ApiState();
 }
 
 class _ApiState extends State<Api> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Future<List<UserInformation>> getData() async {
+    List<UserInformation> users_info_list = [];
+
+    var data = await http.get(Uri.parse(
+        "http://www.json-generator.com/api/json/get/cahDxgaBea?indent=2"));
+
+    var json_data = jsonDecode(data.body);
+
+    for (var user in json_data) {
+      UserInformation user_obj = UserInformation(
+          user['picture'], user['name'], user['company'], user['balance']);
+
+      users_info_list.add(user_obj);
+    }
+
+    return users_info_list;
   }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Container(
+                  child: Text("Something wents wrong"),
+                ));
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: snapshot.data[index].picture,
+                        title: snapshot.data[index].name,
+                        subtitle: snapshot.data[index].company,
+                        trailing: snapshot.data[index].data.balance,
+                      );
+                    });
+              }
+            }));
+  }
+}
+
+class UserInformation {
+  var name, company, picture, balance;
+
+  UserInformation(this.picture, this.name, this.company, this.balance);
 }
